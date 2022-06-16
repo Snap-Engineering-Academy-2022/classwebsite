d9e2ca37d8cc4d0e14ea46d1e45a726e456299c5
Asarel Castellanos <asarelc@gmail.com>
2022-06-16 14:26:29 -0700

    Finished media controls.

---
+++ asarel/index.js
@@ -0,0 +1,34 @@
+// const spotifyEmbed = document.getElementById('spotify_embed').style.display;
+// const controls = document.getElementById('controls').style.display;
+// const indicator = document.getElementById('indicator').style.display;
+
+function changeImage() {
+  var image = document.getElementById("myImage");
+  if (image.src.match("pause")) {
+    image.src = "./icons/play-button.png";
+  } else {
+    image.src = "./icons/pause.png";
+  }
+}
+
+// function changeEmbed(album) {
+//   switch (album) {
+//     case "TPAB":
+//       console.log("TPAB works!");
+//       spotifyEmbed = 'block';
+//       controls = 'none';
+//       indicator = 'none';
+//       break;
+//     case "ChineseFountain":
+//       console.log("Chinse Fountain works!");
+//       break;
+//     case "DoggyStyle":
+//       console.log("Doggystyle works!");
+//       break;
+//     case "BlankFace":
+//       console.log("Blank Face works!");
+//       break;
+//     default:
+//     // code block
+//   }
+// }
