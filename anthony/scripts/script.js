2a04e377a82baaa6239c6e6293f79c6a82d85662
awllms <antcodess@gmail.com>
2022-06-16 13:59:32 -0700

    Add some javascript

--- anthony/scripts/script.js
+++ anthony/scripts/script.js
@@ -20,9 +20,108 @@ const throughthewire = document.getElementById("throughthewire");
 const familybusiness = document.getElementById("familybusiness");
 const lastcall = document.getElementById("lastcall");
 
+const playerMessage = document.getElementById("player-message");
+const audioPlayer = document.getElementById("audio-player");
 
-function playerOpenOnClick(track) {
-    document.getElementById(track).addEventListener('click', () => {
-        console.log(track);
-    })
-}
+const introIframe = '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/7lIr3vVhpDkU5mQEDcnA0S?utm_source=generator&theme=0" width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>'
+const wedontcareIframe = '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/0IW0qaeyxL5Et4UG2MrJKB?utm_source=generator&theme=0" width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>'
+const graduationdayIframe = '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/7wL7Lb8Q3aYyq6gmRL0PZq?utm_source=generator&theme=0" width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>'
+const allfallsdownIframe = '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/7lIr3vVhpDkU5mQEDcnA0S?utm_source=generator&theme=0" width="100%" height="380" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>'
+const illflyawayIframe = '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/7lIr3vVhpDkU5mQEDcnA0S?utm_source=generator&theme=0" width="100%" height="380" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>'
+const spaceshipIframe = '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/1ko2NuvWlQdxtNRc8QQzmT?utm_source=generator&theme=0" width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>'
+const jesuswalksIframe = '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/5g1vtHqi9uV7xtYeCcFOBx?utm_source=generator&theme=0" width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>'
+const neverletmedownIframe = '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/7lIr3vVhpDkU5mQEDcnA0S?utm_source=generator&theme=0" width="100%" height="380" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>'
+const getemhighIframe = '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/5b2FFKC64kxqcfaFnIZxoM?utm_source=generator&theme=0" width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>'
+const workoutplanIframe = '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/7lIr3vVhpDkU5mQEDcnA0S?utm_source=generator&theme=0" width="100%" height="380" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>'
+const thenewworkoutplanIframe = '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/7lIr3vVhpDkU5mQEDcnA0S?utm_source=generator&theme=0" width="100%" height="380" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>'
+
+
+intro.onclick = function() {
+    playerMessage.style.display = "none";
+
+    while (audioPlayer.firstChild) {
+        audioPlayer.removeChild(audioPlayer.firstChild);
+    }
+
+    audioPlayer.innerHTML = introIframe;
+};
+
+wedontcare.onclick = function() {
+    playerMessage.style.display = "none";
+
+    while (audioPlayer.firstChild) {
+        audioPlayer.removeChild(audioPlayer.firstChild);
+    }
+
+    audioPlayer.innerHTML = wedontcareIframe;
+};
+
+graduationday.onclick = function() {
+    playerMessage.style.display = "none";
+
+    while (audioPlayer.firstChild) {
+        audioPlayer.removeChild(audioPlayer.firstChild);
+    }
+
+    audioPlayer.innerHTML = graduationdayIframe;
+};
+
+allfallsdown.onclick = function() {
+    playerMessage.style.display = "none";
+
+    while (audioPlayer.firstChild) {
+        audioPlayer.removeChild(audioPlayer.firstChild);
+    }
+
+    audioPlayer.innerHTML = allfallsdownIframe;
+};
+
+illflyaway.onclick = function() {
+    playerMessage.style.display = "none";
+
+    while (audioPlayer.firstChild) {
+        audioPlayer.removeChild(audioPlayer.firstChild);
+    }
+
+    audioPlayer.innerHTML = illflyawayIframe;
+};
+
+spaceship.onclick = function() {
+    playerMessage.style.display = "none";
+
+    while (audioPlayer.firstChild) {
+        audioPlayer.removeChild(audioPlayer.firstChild);
+    }
+
+    audioPlayer.innerHTML = spaceshipIframe;
+};
+
+jesuswalks.onclick = function() {
+    playerMessage.style.display = "none";
+
+    while (audioPlayer.firstChild) {
+        audioPlayer.removeChild(audioPlayer.firstChild);
+    }
+
+    audioPlayer.innerHTML = jesuswalksIframe;
+};
+
+neverletmedown.onclick = function() {
+    playerMessage.style.display = "none";
+
+    while (audioPlayer.firstChild) {
+        audioPlayer.removeChild(audioPlayer.firstChild);
+    }
+
+    audioPlayer.innerHTML = neverletmedownIframe;
+};
+
+getemhigh.onclick = function() {
+    playerMessage.style.display = "none";
+
+    while (audioPlayer.firstChild) {
+        audioPlayer.removeChild(audioPlayer.firstChild);
+    }
+
+    audioPlayer.innerHTML = getemhighIframe;
+};
